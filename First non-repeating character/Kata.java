8b76e86249e482bfa347eca8422c0bd3f04c15c9
Shreyan Mitra <shreyan.mitra09@gmail.com>
2026-07-12 12:04:06 -0700

    ..

--- First non-repeating character/Kata.java
+++ First non-repeating character/Kata.java
@@ -1,22 +1,33 @@
+import java.util.HashMap;
+
 public class Kata {
   public static String firstNonRepeatingLetter(String s) {
-    int[] cps = s.codePoints().toArray();
+    HashMap<String, Integer> letterCount = new HashMap<>();
 
-    for (int i = 0; i < cps.length; i++) {
-        int cp = Character.toLowerCase(cps[i]);
-
-        int count = 0;
-        for (int j = 0; j < cps.length; j++) {
-            if (Character.toLowerCase(cps[j]) == cp) {
-                count++;
-            }
-        }
-
-        if (count == 1) {
-            return new String(Character.toChars(cps[i]));
-        }
+    for (int i = 0; i < s.length(); i++){
+      String c = s.substring(i, i + 1);
+      if (c.matches("[a-zA-Z]")){
+          c = c.toLowerCase();
+      }
+      if (letterCount.containsKey(c)){
+        letterCount.put(c, letterCount.get(c) + 1);
+      }
+      else{
+        letterCount.put(c, 1);
+      }
     }
 
+    for (int i = 0; i < s.length(); i++){
+      if (letterCount.get(s.substring(i, i + 1)) == 1){
+        return s.substring(i, i + 1);
+      }
+    }
     return "";
-}
+  }
+
+  public static void main(String[] args) {
+      System.out.println(firstNonRepeatingLetter("streSS"));
+      System.out.println(firstNonRepeatingLetter("s"));
+      System.out.println(firstNonRepeatingLetter("moon-men"));
+  }
 }
