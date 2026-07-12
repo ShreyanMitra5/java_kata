232cc3b294ee5128ebc65bfff578d56c8a2c58af
Shreyan Mitra <shreyan.mitra09@gmail.com>
2026-07-12 12:06:41 -0700

    ..

--- First non-repeating character/Kata.java
+++ First non-repeating character/Kata.java
@@ -2,27 +2,34 @@ import java.util.HashMap;
 
 public class Kata {
   public static String firstNonRepeatingLetter(String s) {
-    HashMap<String, Integer> letterCount = new HashMap<>();
+    HashMap<Character, Integer> letterCount = new HashMap<>();
 
-    for (int i = 0; i < s.length(); i++){
-      String c = s.substring(i, i + 1);
-      if (c.matches("[a-zA-Z]")){
-          c = c.toLowerCase();
-      }
-      if (letterCount.containsKey(c)){
-        letterCount.put(c, letterCount.get(c) + 1);
-      }
-      else{
-        letterCount.put(c, 1);
-      }
-    }
+  for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
 
-    for (int i = 0; i < s.length(); i++){
-      if (letterCount.get(s.substring(i, i + 1)) == 1){
-        return s.substring(i, i + 1);
+      if (Character.isLetter(c)) {
+          c = Character.toLowerCase(c);
       }
-    }
-    return "";
+
+      if (letterCount.containsKey(c)) {
+          letterCount.put(c, letterCount.get(c) + 1);
+      } else {
+          letterCount.put(c, 1);
+      }
+  }
+
+  for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+
+      if (Character.isLetter(c)) {
+          c = Character.toLowerCase(c);
+      }
+
+      if (letterCount.get(c) == 1) {
+          return s.substring(i, i + 1);
+      }
+  }
+  return "";
   }
 
   public static void main(String[] args) {
